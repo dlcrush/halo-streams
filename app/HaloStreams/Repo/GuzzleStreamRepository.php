61623bcb0872a9d27fffaaf5106405c56f120c04
David Crush <dlcrush@mail.roanoke.edu>
2015-03-01 21:55:50 -0500

    cleaning up code

--- app/HaloStreams/Repo/GuzzleStreamRepository.php
+++ app/HaloStreams/Repo/GuzzleStreamRepository.php
@@ -2,26 +2,58 @@
 
 class GuzzleStreamRepository implements StreamRepositoryInterface {
 
+	/**
+	 * Guzzle HTTP Client
+	 * @var GuzzleHttp\Client
+	 */
 	protected $client;
 
+	/**
+	 * Constructor
+	 *
+	 * @param \GuzzleHttp\Client $client
+	 */
 	public function __construct(\GuzzleHttp\Client $client) {
 		$this->client = $client;
 	}
 
+	/**
+	 * Gets all streams
+	 *
+	 * @param mixed $limit
+	 * @return stdClass
+	 */
 	public function getAllStreams($limit=null) {
 		return $this->getStreams($limit);
 	}
 
+	/**
+	 * Gets a random stream
+	 *
+	 * @return [type] [description]
+	 */
 	public function getRandomStream() {
 		// @TODO Get random stream
 	}
 
+	/**
+	 * Gets streams
+	 *
+	 * @param  mixed  $limit
+	 * @param  integer $offset
+	 * @return 	 stdClass
+	 */
 	public function getStreams($limit=null,$offset=0) {
 		return $this->client->get('https://api.twitch.tv/kraken/streams?limit=' . $limit . '&offset=' . $offset,
 				['query' => ['game' => 'Halo: The Master Chief Collection'], ['embeddable' => 'true']])->json(['object' => true]);
 	}
 
-	public function getStream($id) {
+	/**
+	 *  Get stream
+	 * @param  int    $id
+	 * @return  stdClass
+	 */
+	public function getStream(int $id) {
 		return $stream = $this->client->get('https://api.twitch.tv/kraken/streams/' . $id)->json(['object' => true]);
 	}
 
