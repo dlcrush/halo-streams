b82b6af01ca54ea74ac2bf304922a1142038b979
David Crush <dlcrush@mail.roanoke.edu>
2015-04-20 09:23:40 -0400

    updated ads

--- resources/views/streams/partials/_ads.blade.php
+++ resources/views/streams/partials/_ads.blade.php
@@ -1,12 +1,10 @@
-
 <script type="text/javascript">
   ( function() {
     if (window.CHITIKA === undefined) { window.CHITIKA = { 'units' : [] }; };
-    var unit = {"calltype":"async[2]","publisher":"halostreams","width":728,"height":90,"sid":"Chitika Default","color_site_link":"ffffff","color_text":"cccccc","color_bg":"3F5353","color_button":"121f1f","color_button_text":"cccccc"};
+    var unit = {"calltype":"async[2]","publisher":"halostreams","width":550,"height":250,"sid":"Chitika Default","color_site_link":"ffffff","color_text":"cccccc","color_bg":"3F5353","color_button":"121f1f","color_button_text":"cccccc"};
     var placement_id = window.CHITIKA.units.length;
     window.CHITIKA.units.push(unit);
     document.write('<div id="chitikaAdBlock-' + placement_id + '"></div>');
 }());
 </script>
 <script type="text/javascript" src="//cdn.chitika.net/getads.js" async></script>
-
