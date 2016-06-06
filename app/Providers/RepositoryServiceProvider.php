f81a55af57639ace92006fc613cfeca174b6daba
David Crush <dlcrush@mail.roanoke.edu>
2016-06-05 21:09:11 -0400

    damn

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -11,6 +11,7 @@ class RepositoryServiceProvider extends ServiceProvider {
      */
     public function register() {
         $this->app->bind('App\Repositories\Contracts\StreamRepositoryInterface', function($app) {
+        				$clientID = Config::get('twitch.clientID');
             return new \App\Repositories\GuzzleStreamRepository(new \GuzzleHttp\Client);
         });
     }
